d68affa2f65e64023a3395776426dde2b8373f44
mcnoob12 <119181559+mcnoob12@users.noreply.github.com>
2023-11-18 12:35:26 +0530

    Updating

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-function know() {
-    window.location.href = "information.html";
-}
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener("mousemove", function (e){
+
+    const posX = e.clientX
+    const posy = e.clientY
+
+    cursorDot.style.left = `${posX}px`
+    cursorDot.style.top =  `${posY}px`
+
+    cursorOutline.style.left = `${posX}px`
+    cursorOutline.style.top =  `${posY}px`
+
+
+
+});
